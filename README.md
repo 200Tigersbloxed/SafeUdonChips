b8949ecd543571f5ebc26696318c4eca93067b70
200Tigersbloxed <45884377+200Tigersbloxed@users.noreply.github.com>
2025-01-10 15:40:26 -0500

    v1.0.0 initial release

---
+++ README.md
@@ -0,0 +1,76 @@
+# SafeUdonChips
+
+A feature rich plug-in for [UdonChips](https://lura.booth.pm/items/3060394) which adds configurable persistence.
+
+## Setup
+
+1. Download the latest Release
+2. Import the unitypackage into your project
+3. Drag the Prefab into the Scene with the UdonChips GameObject
+4. Reference the Chips object
+5. *(Optional)* Configure the Behaviour
+
+## Configuration
+
+Below are the possible configurations and their definitions.
+
+### Chips
+
+> [!CAUTION]
+> 
+> This field is **REQUIRED** to be set. SafeChips will not run without this field.
+
+The GameObject which contains the UdonChips UdonBehaviour.
+
+### CheckTime
+
+The event frame to continuously check for changes to the Money value on UdonChips.
+
++ [FixedUpdate](https://docs.unity3d.com/2022.3/Documentation/ScriptReference/MonoBehaviour.FixedUpdate.html)
++ [Update](https://docs.unity3d.com/2022.3/Documentation/ScriptReference/MonoBehaviour.Update.html) *(default)*
++ [LateUpdate](https://docs.unity3d.com/2022.3/Documentation/ScriptReference/MonoBehaviour.LateUpdate.html)
++ Interval
+  + Checks for changes in an interval measured in seconds
++ Scripted
+  + Only invokes when messaged `_RefreshChipData`
+
+> [!IMPORTANT]
+> 
+> When **NOT** using `Scripted` CheckTime, you **CANNOT** invoke `_RefreshChipData` to update money.
+
+### IntervalRate
+
+> [!NOTE]
+> 
+> This configuration is only valid when CheckTime is set to Interval
+
+The rate of when to check again for changes to the Money value on UdonChips. Measured in seconds.
+
+### DifferenceTolerance
+
+The tolerance amount for how different the difference of money has to be before updating again. Recommended to keep at `0.001f`, only change if your money is expected to go past 3 decimal places.
+
+> [!WARNING]
+> 
+> The Advanced configurations are not meant to be messed with. Please only change these if you have a valid reason to.
+
+### MoneyKey (Advanced)
+
+The key to use when saving money to PlayerData.
+
+### HasInitializedKey (Advanced)
+
+The key to use when saving initialization checks to PlayerData.
+
+## Multiple Chips?
+
+In theory, this project will work fine with multiple UdonChips Behaviours as long as you can get them to work together. Be sure that there are no duplicate keys for PlayerData (see MoneyKey and HasInitializedKey configurations).
+
+## Messages
+
+The following section goes over messages that are available to developers who want to interface with SafeUdonChips.
+
++ _RefreshChipData
+  + Checks for change in Money when using Scripted CheckTime
++ _ResetData
+  + Resets all PlayerData and sets Money to the starting value
---
+++ SafeChips.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+using UCS;
+using UdonSharp;
+using UnityEngine;
+using VRC.SDK3.Persistence;
+using VRC.SDKBase;
+
+public class SafeChips : UdonSharpBehaviour
+{
+    private const string VERSION = "v1.0.0";
+    
+    [NotNull]
+    [Tooltip("(Required) The UdonChips object to associate with this instance")]
+    public UdonChips Chips;
+    [Tooltip("When to run the checks for when money changes")]
+    public CheckType CheckTime = CheckType.Update;
+    [Tooltip("(When CheckTime is set to Interval) The amount of time in seconds to check for money changes")]
+    public float IntervalRate = 3f;
+    [Tooltip("How much of a difference there must be from the previous check to the current check to save money")]
+    public float DifferenceTolerance = 0.001f;
+    [Tooltip("(Advanced) What the key for saving money with persistence should be")]
+    public string MoneyKey = "Money";
+    [Tooltip("(Advanced) What the key for checking initialize status with persistence should be")]
+    public string HasInitializedKey = "HasInit";
+    
+    private bool noChips = true;
+    private bool dontRun = true;
+    private bool firstLoad;
+    private bool sentWaitEvent;
+    private float lastChipValue;
+    private float startingMoney;
+
+    /// <summary>
+    /// The Event Message to Invoke when CheckTime is set to Scripted
+    /// </summary>
+    public void _RefreshChipData()
+    {
+        if(CheckTime != CheckType.Scripted)
+        {
+#if DEBUG
+            Debug.LogWarning("Tried to RefreshChipData when not Scripting!");
+#endif
+            return;
+        }
+        Check();
+    }
+
+    /// <summary>
+    /// The Event Message to Invoke when you want to Reset Saved Data
+    /// </summary>
+    public void _ResetData()
+    {
+        PlayerData.SetFloat(MoneyKey, startingMoney);
+        Chips.money = startingMoney;
+        lastChipValue = startingMoney;
+#if DEBUG
+        Debug.LogWarning("Reset player data!");
+#endif
+    }
+    
+    public void _IntervalWaitEvent()
+    {
+        // Don't run or CheckTime is not Interval or we haven't requested a sentWaitEvent
+        if(dontRun || CheckTime != CheckType.Interval || !sentWaitEvent)
+        {
+            // Sanity Check
+            if(dontRun) return;
+            // If CheckTime is not Interval, but we are waiting for an event, no we are not
+            if(CheckTime != CheckType.Interval && sentWaitEvent) sentWaitEvent = false;
+            return;
+        }
+        Check();
+        sentWaitEvent = false;
+    }
+
+    private void Check()
+    {
+        // If SafeChips couldn't load, don't do anything
+        if(dontRun) return;
+        // Values are not the same, don't do anything
+        if(Math.Abs(lastChipValue - Chips.money) < DifferenceTolerance) return;
+        // Values changed, so it's time for an update
+        PlayerData.SetFloat(MoneyKey, Chips.money);
+        lastChipValue = Chips.money;
+#if DEBUG
+        Debug.Log("Updated Money to " + lastChipValue);
+#endif
+    }
+
+    private void CheckIntervalStatus()
+    {
+        // Don't run or CheckTime is not Interval or we already sent the wait event
+        if(dontRun || CheckTime != CheckType.Interval || sentWaitEvent) return;
+        SendCustomEventDelayedSeconds(nameof(_IntervalWaitEvent), IntervalRate);
+        sentWaitEvent = true;
+    }
+
+    private void Start()
+    {
+        if (Chips == null)
+        {
+            Debug.LogError("Cannot load SafeChips! No Chips object was provided.");
+            return;
+        }
+        // Initialize Money
+        startingMoney = Chips.money;
+        noChips = false;
+    }
+
+    public override void OnPlayerDataUpdated(VRCPlayerApi player, PlayerData.Info[] infos)
+    {
+        // Don't do anything if the data isn't local, there's no Chips object, or we've already done a PlayerData load
+        if(!player.isLocal || noChips || firstLoad) return;
+        // Initialize Data Store
+        float loadedMoney = startingMoney;
+        bool hasInit = PlayerData.GetBool(Networking.LocalPlayer, HasInitializedKey);
+        if(hasInit)
+        {
+            float currentMoney = PlayerData.GetFloat(Networking.LocalPlayer, MoneyKey);
+            loadedMoney = currentMoney;
+            Chips.money = currentMoney;
+        }
+        else
+        {
+            PlayerData.SetFloat(MoneyKey, startingMoney);
+            Chips.money = startingMoney;
+            PlayerData.SetBool(HasInitializedKey, true);
+#if DEBUG
+            Debug.Log("Created PlayerData!");
+#endif
+        }
+        lastChipValue = loadedMoney;
+        dontRun = false;
+        firstLoad = true;
+#if DEBUG
+        Debug.Log($"Loaded SafeChips ({VERSION}) with {loadedMoney} UdonChips for {player.playerId}!");
+#endif
+    }
+
+    private void FixedUpdate()
+    {
+        if(CheckTime != CheckType.FixedUpdate) return;
+        Check();
+    }
+    
+    private void Update()
+    {
+        CheckIntervalStatus();
+        if(CheckTime != CheckType.Update) return;
+        Check();
+    }
+    
+    private void LateUpdate()
+    {
+        if(CheckTime != CheckType.LateUpdate) return;
+        Check();
+    }
+}
+
+public enum CheckType
+{
+    FixedUpdate,
+    Update,
+    LateUpdate,
+    Interval,
+    Scripted
+}
